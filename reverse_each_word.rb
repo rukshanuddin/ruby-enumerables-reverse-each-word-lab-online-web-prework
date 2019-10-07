4a3338e7aa4fae7511b2e3a552fdba8d14fa8b14
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-07 02:05:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(sentence1)
-  s = sentence1.split(/ /)
-  d = s.collect { |e| "#{e.reverse}" }
-  d.join(" ")
-end
+#def reverse_each_word(sentence1)
+#  s = sentence1.split(/ /)
+#  d = s.collect { |e| "#{e.reverse}" }
+#  d.join(" ")
+#end
 
 def reverse_each_word(sentence1)
   sentence1.split.collect { |e| "#{e.reverse}" }.join(" ")
